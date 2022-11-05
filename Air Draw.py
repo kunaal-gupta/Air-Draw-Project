9400f74b7dbb7accdeb994c56a4ed235c53885bf
Kunaal Gupta <87749508+kunaal-gupta@users.noreply.github.com>
2022-11-05 13:53:43 -0600

    track successful

--- Air Draw.py
+++ Air Draw.py
@@ -1,15 +1,16 @@
 import cv2 as cv
 import numpy as np
 
-capture = cv.VideoCapture(0)  # Either path of video file you wanna play or int 0,1,2 for webcam access
+capture = cv.VideoCapture(0)
+# cv.flip()# Either path of video file you wanna play or int 0,1,2 for webcam access
 
-capture.set(3, 1024)  # height
-capture.set(4, 1024)  # width
-capture.set(10, 130)  # brightness
+capture.set(3, 500)  # height
+capture.set(4, 500)  # width
+capture.set(10, 100)  # brightness
 
-myColors = [[0, 120, 177, 177, 255, 255]]
+myColors = [[0, 80, 231, 179, 208, 255]]
 
-myColorValues = [[51, 153, 255]]
+myColorValues = [[51, 53, 255]]
 
 myPoints = []  ## [x, y, colorId]
 
@@ -26,7 +27,7 @@ def findColor(img, myColors, myColorValuess):
         cv.circle(imgResult, (x, y), 10, myColorValues[0], cv.FILLED)
         if x != 0 and y != 0:
             newPoints.append([x, y, count])
-        count +=1
+        count += 1
     # cv.imshow('image', mask)
 
     return newPoints
@@ -38,20 +39,21 @@ def getContours(img):
     for cnt in contours:
         area = cv.contourArea(cnt)
         if area > 500:
-            # cv.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
+            cv.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
             peri = cv.arcLength(cnt, True)
             approx = cv.approxPolyDP(cnt, 0.02 * peri, True)
             x, y, w, h = cv.boundingRect(approx)
-    return x + w // 2, y
+    return (x + w // 2), y
 
 
 def drawOnCanvus(myPoints, myColorValues):
     for point in myPoints:
-        cv.circle(imgResult, (point[0], point[1]), 10, myColorValues[point[2]], cv.FILLED)
+        cv.circle(imgResult, (point[0], point[1]), 5, myColorValues[point[2]],10, lineType=-1)
 
 
 while True:
-    isTrue, img = capture.read()  # Captures the video frame by frame & isTrue bolean indicating whether it's successful or not
+    isTrue, img = capture.read()  # Captures the video frame by frame & isTrue boolean indicating whether it's successful or not
+    imgFlip = cv.flip(img, 1)
     imgResult = img.copy()
     newPoints = findColor(img, myColors, myColorValues)
     if len(newPoints) != 0:
@@ -61,9 +63,8 @@ while True:
     if len(myPoints) != 0:
         drawOnCanvus(myPoints, myColorValues)
 
-
     cv.imshow('Result', imgResult)  # Displaying video as a new window (windoow name, img)
-    if cv.waitKey(20) & 0xFF == ord('Q'):  # break the loop if key 'c' is pressed
+    if cv.waitKey(1) & 0xFF == ord('Q'):  # break the loop if key 'c' is pressed
         break
 
 capture.release()
